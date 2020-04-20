97892048d3c04fb51f0acbbfa6117af10848e8a8
Enjoyone <1693364356@qq.com>
2020-04-20 17:48:18 +0800

    delete security

--- src/main/java/com/liu/blog/controller/WebSecurityConfig.java
+++ src/main/java/com/liu/blog/controller/WebSecurityConfig.java
@@ -2,30 +2,30 @@ package com.liu.blog.controller;
 
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.builders.WebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class WebSecurityConfig  extends WebSecurityConfigurerAdapter{
+public class WebSecurityConfig  {
 
+//extends WebSecurityConfigurerAdapter
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
 //
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http.authorizeRequests().antMatchers("/", "/index", "/showArticle/**", "/register/**","/info/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login").permitAll()
-                .and()
-                .logout().permitAll();
-    }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/css/**", "/js/**", "/image/**", "/res/**","/h2-console/**");
-    }
+//        http.authorizeRequests().antMatchers("/", "/index", "/showArticle/**", "/register/**","/info/**").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .loginPage("/login").permitAll()
+//                .and()
+//                .logout().permitAll();
+//    }
+//
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        web.ignoring().antMatchers("/css/**", "/js/**", "/image/**", "/res/**","/h2-console/**");
+//    }
 
 
 }
